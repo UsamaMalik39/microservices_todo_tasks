90fe424b3322fcd0d945cf68e3d54f9679318417
malik39 <fusama93@gmail.com>
2024-01-20 13:35:58 +0500

    deleted extra files

--- notification-platform/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { NotifyModule } from './api/notify.module';
-
-@Module({
-  imports: [NotifyModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- notification-platform/src/common/config/app.config.ts
+++
@@ -1,3 +0,0 @@
-export const MAIL_SERVICE_HOST = process.env.MAIL_SERVICE_HOST ?? '0.0.0.0';
-export const MAIL_SERVICE_PORT = process.env.MAIL_SERVICE_PORT ?? 3002;
-export const MAILER_DISABLED = process.env.MAILER_DISABLED ?? false;
