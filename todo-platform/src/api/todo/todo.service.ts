66b861a6a8d7d9ce59446895b46a32c96a5e2001
malik39 <fusama93@gmail.com>
2024-01-20 17:01:13 +0500

    status check applied

--- todo-platform/src/api/todo/todo.service.ts
+++ todo-platform/src/api/todo/todo.service.ts
@@ -1,9 +1,10 @@
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, Logger } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { TodoDto, TodoRequestDto } from 'src/api/dto/todo.dto';
 import { UserDto } from 'src/api/dto/user.dto';
 import { USER_SERVICE } from 'src/common/config/constant';
 import { PermissionGuard } from 'src/common/permission.guard';
+import { TaskStatus } from 'src/repositories/model/status';
 import { IUser } from 'src/repositories/model/user';
 import { TodoRepository } from 'src/repositories/todo.repository';
 
@@ -32,11 +33,17 @@ export class TodoService {
     request: TodoRequestDto,
     loggedInUser: UserDto,
   ): Promise<TodoDto> {
-    const todo: TodoDto = {
-      ...request,
-      active: true,
-    };
-    return await this.todoRepository.create(todo, loggedInUser);
+    if(Object.values(TaskStatus).includes(request.status as TaskStatus)){
+      const todo: TodoDto = {
+        ...request,
+        active: true,
+      };
+      return await this.todoRepository.create(todo, loggedInUser);
+    }
+    else{
+      throw new BadRequestException();
+    } 
+    
   }
 
   public async update(
@@ -44,14 +51,20 @@ export class TodoService {
     request: TodoRequestDto,
     loggedInUser: UserDto,
   ): Promise<TodoDto> {
-    const dbRecord = await this.todoRepository.findOne(id);
-    this.permissionGuard.hasPermission(dbRecord, loggedInUser);
-    this.permissionGuard.isTodoActive(dbRecord);
-    const todo: TodoDto = {
-      ...request,
-      active: dbRecord.active,
-    };
-    return await this.todoRepository.update(id, todo, loggedInUser);
+    if(Object.values(TaskStatus).includes(request.status as TaskStatus)){
+      const dbRecord = await this.todoRepository.findOne(id);
+      this.permissionGuard.hasPermission(dbRecord, loggedInUser);
+      this.permissionGuard.isTodoActive(dbRecord);
+      const todo: TodoDto = {
+        ...request,
+        active: dbRecord.active,
+      };
+      return await this.todoRepository.update(id, todo, loggedInUser);
+    }
+    else{
+      throw new BadRequestException();
+    }
+
   }
 
   public async delete(id: string, loggedInUser: UserDto): Promise<TodoDto> {
